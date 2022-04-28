41ddf80f2674af808727688a8dd43c1d6e36620e
Patrick McLaughlin <patrickmclaughlin@bitgo.com>
2022-04-28 14:26:18 -0400

    chore: fix inconsistent file names in io-ts-http

--- packages/io-ts-http/src/index.ts
+++ packages/io-ts-http/src/index.ts
@@ -3,9 +3,8 @@
  * Codecs for (de)serializing HTTP requests from both the client and server side
  */
 
-export * from './array-from-non-empty-delimited-string';
 export * from './combinators';
 export * from './httpResponse';
 export * from './httpRequest';
 export * from './httpRoute';
-export * from './query-params';
+export * from './queryParams';
--- packages/io-ts-http/src/queryParams.ts
+++ packages/io-ts-http/src/queryParams.ts
@@ -1,10 +1,38 @@
+import { pipe } from 'fp-ts/function';
+import * as E from 'fp-ts/Either';
 import { NonEmptyArray } from 'fp-ts/NonEmptyArray';
 import * as t from 'io-ts';
-import { nonEmptyArray } from 'io-ts-types';
-
-import { arrayFromNonEmptyCommaDelimitedString } from './array-from-non-empty-delimited-string';
+import { nonEmptyArray, NonEmptyString } from 'io-ts-types';
 
 export type { NonEmptyArray } from 'fp-ts/NonEmptyArray';
+
+function arrayFromNonEmptyDelimitedString(
+  delimiter: string,
+  delimiterName: string,
+): <C extends t.Type<any, string>>(
+  codec: C,
+  codecName?: string,
+) => t.Type<NonEmptyArray<t.TypeOf<C>>, string> {
+  return <C extends t.Type<any, string>>(codec: C, codecName: string = codec.name) => {
+    const neaCodec = nonEmptyArray(codec);
+    return new t.Type<NonEmptyArray<t.TypeOf<C>>, string>(
+      `ArrayFromNonEmpty${delimiterName}DelimitedString(${codecName})`,
+      neaCodec.is,
+      (input, context) =>
+        pipe(
+          NonEmptyString.validate(input, context),
+          E.map((s) => s.split(delimiter)),
+          E.chain((arr) => neaCodec.validate(arr, context)),
+        ),
+      (a) => a.map(codec.encode).join(delimiter),
+    );
+  };
+}
+
+const arrayFromNonEmptyCommaDelimitedString = arrayFromNonEmptyDelimitedString(
+  ',',
+  'Comma',
+);
 
 const emptyArrayFromUndefined = <C>() =>
   new t.Type(
