c4729affcc57f61bfb775bb6696efbbd7c7917c6
Patrick McLaughlin <patrickmclaughlin@bitgo.com>
2022-05-11 14:05:40 -0400

    feat(nix): use flake

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "api-ts";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, flake-compat }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in {
+          devShell = pkgs.mkShell {
+            name = "api-ts-shell";
+
+            packages = with pkgs; [
+              nodejs-16_x
+              go
+            ];
+
+            shellHook = ''
+              export PATH="$(pwd)/node_modules/.bin:$PATH"
+            '';
+          };
+        }
+      );
+}
