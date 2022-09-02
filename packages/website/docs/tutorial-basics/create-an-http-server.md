22c1962ea1cc33ed8b2b9ac55daff4afc92674d1
Eric Crosson <ericcrosson@bitgo.com>
2022-09-02 16:32:36 -0500

    chore: add GitHub Pages
    To demo the site locally, run:
    
    ```
    npm ci
    npm --prefix packages/website start
    ```
    
    This is a _heavy_ WIP but I'd also like to get this into a
    continously-deploying state quickly, to prevent it from rotting too
    long internally.
    
    I plan to contribute regularly to these docs, so regardless of the
    starting state we should have a good trajectory.

---
+++ packages/website/docs/tutorial-basics/create-an-http-server.md
@@ -0,0 +1,85 @@
+---
+sidebar_position: 2
+---
+
+# Create an HTTP Server
+
+An API specification is only useful when clients and servers adhere to its rules.
+
+`io-ts-http` API specifications are not coupled to any particular HTTP server. `api-ts`
+provides helper libraries that wrap your favorite web server and use the TypeScript
+type-checker to ensure your server implementation satisfies the target API
+specification.
+
+## Create an HTTP server implementing your API specification
+
+We'll use [express] as our underlying web server in this tutorial.
+
+[express]: https://github.com/expressjs/express
+
+First, edit your `package.json` file to add a few new dependencies (highlighted):
+
+```json package.json focus=7,9,13,14
+{
+  "name": "api-ts-example",
+  "scripts": {
+    "build": "tsc --lib es2015 --esModuleInterop *.ts"
+  },
+  "dependencies": {
+    "@api-ts/express-wrapper": "1.0.0-beta.20",
+    "@api-ts/io-ts-http": "0.2.0-beta.9",
+    "@api-ts/response": "0.1.2-beta.2",
+    "io-ts": "2.1.3"
+  },
+  "devDependencies": {
+    "@types/express": "4.17.13",
+    "@types/node": "18.6.1",
+    "typescript": "4.7.4"
+  }
+}
+```
+
+And install them by running:
+
+```
+$ npm install
+```
+
+Next, create a new file `server.ts`:
+
+```typescript server.ts
+import { Response } from '@api-ts/response';
+import { createServer } from '@api-ts/express-wrapper';
+
+import { API } from './index';
+
+const PORT = 3000;
+
+const hello = (parameters: { name: string }) => {
+  return Response.ok(`Hello, ${parameters.name}!`);
+};
+
+const app = createServer(API, () => {
+  return {
+    'api.v1.hello': {
+      get: hello,
+    },
+  };
+});
+
+app.listen(PORT, () => `Server is listening at http://localhost:${PORT}`);
+```
+
+Compile and run `server.ts`:
+
+```
+$ npm run build
+$ node ./server.js
+```
+
+Finally, submit an HTTP request to your server in a web browser or using a new terminal:
+
+```
+$ curl localhost:3000/hello/world
+"Hello, world!"
+```
---
+++ packages/website/docusaurus.config.js
@@ -0,0 +1,132 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+const theme = require("shiki/themes/nord.json")
+const {
+  remarkCodeHike,
+} = require("@code-hike/mdx")
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'api-ts home',
+  tagline: 'Type- and runtime- safe TypeScript APIs',
+  url: 'https://bitgo.github.io/api-ts/',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'BitGo', // Usually your GitHub org/user name.
+  projectName: 'api-ts', // Usually your repo name.
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          beforeDefaultRemarkPlugins: [
+            [remarkCodeHike, { theme }],
+          ],
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        theme: {
+          customCss: [
+            require.resolve("@code-hike/mdx/styles.css"),
+            require.resolve('./src/css/custom.css'),
+          ]
+        },
+      }),
+    ],
+  ],
+
+  themes: ["mdx-v2"],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'My Site',
+        logo: {
+          alt: 'My Site Logo',
+          src: 'img/logo.svg',
+        },
+        items: [
+          {
+            type: 'doc',
+            docId: 'intro',
+            position: 'left',
+            label: 'Tutorial',
+          },
+          {
+            href: 'https://github.com/facebook/docusaurus',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Tutorial',
+                to: '/docs/intro',
+              },
+            ],
+          },
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'Stack Overflow',
+                href: 'https://stackoverflow.com/questions/tagged/api-ts',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'GitHub',
+                href: 'https://github.com/BitGo/api-ts',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
