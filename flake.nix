5fb8a2f76e5e6611e92876ff192a66a53acdcdd5
Eric Crosson <ericcrosson@bitgo.com>
2022-11-21 09:20:28 -0600

    chore(nix): remove flake-compat
    This PR accepts peer-review feedback from #308
    
    Co-authored-by: Louis Varin <louisvarin@bitgo.com>

--- flake.nix
+++ flake.nix
@@ -4,17 +4,12 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
-    flake-compat = {
-      url = "github:edolstra/flake-compat";
-      flake = false;
-    };
   };
 
   outputs = {
     self,
     nixpkgs,
     flake-utils,
-    flake-compat,
   }: (
     flake-utils.lib.eachDefaultSystem (
       system: (
