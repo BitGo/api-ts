7638329906127fa4e4d54af06c5c488aa0fb729c
Aryaman Dhingra <aryamandhingra@bitgo.com>
2024-08-06 09:02:13 -0400

    test: add back missing test
    DX-647

--- packages/openapi-generator/test/openapi/ref.test.ts
+++ packages/openapi-generator/test/openapi/ref.test.ts
@@ -304,3 +304,151 @@ testCase('request body nullable ref', SCHEMA_NULLABLE_REF, {
     },
   },
 });
+
+
+const ROUTE_WITH_SCHEMA_WITH_COMMENT = `
+import * as t from 'io-ts';
+import * as h from '@api-ts/io-ts-http';
+/**
+ * A simple route with type descriptions for references
+ *
+ * @operationId api.v1.test
+ * @tag Test Routes
+ */
+export const route = h.httpRoute({
+  path: '/foo',
+  method: 'GET',
+  request: h.httpRequest({}),
+  response: {
+    200: SimpleRouteResponse,
+    400: ApiError,
+    401: InvalidError
+  },
+ });
+/**
+ * Human readable description of the Simple Route Response
+ * @title Human Readable Simple Route Response
+ */
+const SimpleRouteResponse = t.type({
+  test: t.string,
+});
+/**
+ * Human readable description of the InvalidError schema
+ * @title Human Readable Invalid Error Schema
+ */
+const InvalidError = t.intersection([
+  ApiError, 
+  t.type({ error: t.literal('invalid') })]);
+/**
+ * Human readable description of the ApiError schema
+ * @title Human Readable Api Error Schema
+ */
+const ApiError = t.type({
+  error: t.string,
+});
+ `;
+
+testCase('route with api error schema', ROUTE_WITH_SCHEMA_WITH_COMMENT, {
+  openapi: '3.0.3',
+  info: {
+    title: 'Test',
+    version: '1.0.0'
+  },
+  paths: {
+    '/foo': {
+      get: {
+        summary: 'A simple route with type descriptions for references',
+        operationId: 'api.v1.test',
+        tags: [
+          'Test Routes'
+        ],
+        parameters: [],
+        responses: {
+          '200': {
+            description: 'OK',
+            content: {
+              'application/json': {
+                schema: {
+                  '$ref': '#/components/schemas/SimpleRouteResponse'
+                }
+              }
+            }
+          },
+          '400': {
+            content: {
+              'application/json': {
+                schema: {
+                  '$ref': '#/components/schemas/ApiError'
+                }
+              }
+            },
+            description: 'Bad Request'
+          },
+          '401': {
+            description: 'Unauthorized',
+            content: {
+              'application/json': {
+                schema: {
+                  $ref: '#/components/schemas/InvalidError'
+                }
+              }
+            }
+          }
+        }
+      }
+    },
+  },
+  components: {
+    schemas: {
+      ApiError: {
+        properties: {
+          error: {
+            type: 'string'
+          }
+        },
+        required: [
+          'error'
+        ],
+        title: 'Human Readable Api Error Schema',
+        description: 'Human readable description of the ApiError schema',
+        type: 'object'
+      },
+      SimpleRouteResponse: {
+        description: 'Human readable description of the Simple Route Response',
+        properties: {
+          test: {
+            type: 'string'
+          }
+        },
+        required: [
+          'test'
+        ],
+        title: 'Human Readable Simple Route Response',
+        type: 'object',
+      },
+      InvalidError: {
+        title: 'Human Readable Invalid Error Schema',
+        description: 'Human readable description of the InvalidError schema',
+        allOf: [
+          {
+            type: 'object',
+            properties: {
+              error: {
+                type: 'string',
+                enum: [
+                  'invalid'
+                ]
+              }
+            },
+            required: [
+              'error'
+            ]
+          },
+          {
+            $ref: '#/components/schemas/ApiError'
+          }
+        ],
+      },
+    }
+  }
+});
